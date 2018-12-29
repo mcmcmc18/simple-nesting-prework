9d56191a75f1f7e701a209b2996f9cf72d6a5862
MC Simoes <mariacristina.simoes@gmail.com>
2018-12-29 00:35:31 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -103,6 +103,7 @@ def changing_alan
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
      programmer_hash[:alan_kay][:known_for][1] = ["GUI"]
+     return programmer_hash
      
      
 end
@@ -124,5 +125,6 @@ def adding_to_dennis
         }
      }
      programmer_hash[:dennis_ritchie][:languages][1]= ["Assembly"]
+     return programmer_hash
 
 end
